4d0a29ee4f5a5da841c1a84d8780cba298846887
daniedraghici <danieladraghici41@gmail.com>
2025-06-02 22:42:53 +0300

    create pacineti view

--- Web App/WelcomeMedic.js
+++ Web App/WelcomeMedic.js
@@ -1,33 +1,10 @@
-var logout = document.querySelector('#logout');
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      const uid = user.uid;
-      var email = user.email;
-      console.log(email);
-      var userType = user.email.toString().charAt(0);
-      // ...
-    } else {
-      // User is signed out
-      // ...
-      console.log('Not signed in')
-    }
-  });
-logout.addEventListener('submit', (e) => {
+document.querySelector('#logout').addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('in eventlistener');
-    //const auth = getAuth();
-    signOut(auth).then(() => {
-        document.location = 'index.html';
-    }).catch((error) => {
-        console.log('Not logged in');
-    });
+
+    // Curăță datele locale
+    localStorage.removeItem('userType');
+
+    // Redirecționează
+    window.location.href = 'index.html';
 });
-
-
-
-
-
-
-
-
