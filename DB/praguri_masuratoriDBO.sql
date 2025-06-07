276a1748dd309f475cea573e2e74dc01f2250918
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2025-06-07 14:52:26 +0300

    Update praguri_masuratoriDBO.sql

--- DB/praguri_masuratoriDBO.sql
+++ DB/praguri_masuratoriDBO.sql
@@ -1,4 +1,4 @@
-/****** Object:  Table [dbo].[praguri_masuratori]    Script Date: 23.05.2025 18:29:01 ******/
+/****** Object:  Table [dbo].[praguri_masuratori]    Script Date: 07.06.2025 14:51:49 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -7,13 +7,32 @@ GO
 
 CREATE TABLE [dbo].[praguri_masuratori](
 	[id] [int] NOT NULL,
-	[tip_masurare] [varchar](100) NOT NULL,
-	[limita_minima] [numeric](10, 2) NOT NULL,
-	[limita_maxima] [numeric](10, 2) NOT NULL,
+	[cnp_pacient] [char](13) NOT NULL,
+	[limita_minima_puls] [numeric](10, 2) NOT NULL,
+	[limita_maxima_puls] [numeric](10, 2) NOT NULL,
+	[limita_minima_temperatura] [numeric](10, 2) NOT NULL,
+	[limita_maxima_temperatura] [numeric](10, 2) NOT NULL,
+	[limita_minima_ekg] [numeric](10, 2) NOT NULL,
+	[limita_maxima_ekg] [numeric](10, 2) NOT NULL,
+	[cnp_doctor] [char](13) NOT NULL,
  CONSTRAINT [PK_praguri_masuratori] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[praguri_masuratori]  WITH CHECK ADD  CONSTRAINT [FK_praguri_masuratori_pacienti] FOREIGN KEY([cnp_pacient])
+REFERENCES [dbo].[pacienti] ([CNP])
+GO
+
+ALTER TABLE [dbo].[praguri_masuratori] CHECK CONSTRAINT [FK_praguri_masuratori_pacienti]
+GO
+
+ALTER TABLE [dbo].[praguri_masuratori]  WITH CHECK ADD  CONSTRAINT [FK_praguri_masuratori_utilizatori] FOREIGN KEY([cnp_doctor])
+REFERENCES [dbo].[utilizatori] ([CNP])
+GO
+
+ALTER TABLE [dbo].[praguri_masuratori] CHECK CONSTRAINT [FK_praguri_masuratori_utilizatori]
 GO
 
